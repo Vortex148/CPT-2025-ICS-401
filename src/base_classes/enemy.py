111f71976db7373fe11cd08500866ac342650b94
Vortex148 <95885247+Vortex148@users.noreply.github.com>
2024-11-22 11:03:39 -0500

    Feat: Added support for trajectory following !!DO NOT MERGE TO MAIN!! !!VERY UNSTABLE AND UNOPTIMIZED!!

--- src/base_classes/enemy.py
+++ src/base_classes/enemy.py
@@ -1,39 +1,46 @@
 import pygame
-import json
-import src.tools.unit_handler
-from src.tools.unit_handler import swth_sprite
 
-pygame.init()
+from src.tools.time_handler import Timer
+from src.tools.unit_handler import swth_sprite
+from src.tools.trajectory_handler import trajectory_handler
+
 
 # queue_file = open("src/JSON_Files/enemy_queue.json")
 # queue = json.load(queue_file)
 # queue = json.load(queue_file)
 
 enemy_count = 0
-class Enemy(swth_sprite):
-    def __init__(self, position, image, size=(30,30)):
 
+class Enemy(swth_sprite):
+    def __init__(self, image, size=(30,30)):
+        global enemy_count
         self.image = pygame.image.load(image).convert_alpha()
         self.image = pygame.transform.scale(self.image, size)
         super().__init__(self.image)
-        self.rect = self.image.get_rect(center=position)
+        self.traj_handle = trajectory_handler(30, 0, 180, [0, 0], super().get_rect(), self)
         self.active = False # Ensuring the enemies aren't drawn until it is necessary to load them
         # This is further managed using a queue in the attached json file.
-
+        enemy_count += 1
+        self.id = enemy_count
     def activate(self):
         self.active = True
 
-    def update(self):
-        if self.active:
-            pass # code for enemy animation goes here
+    # def update(self):
+    #     if self.active:
+    #         pass # code for enemy animation goes here
+
+    def follow_trajectory(self, trajectory):
+        super().update_position(self.traj_handle.follow_trajectory(trajectory, super().get_position()))
+
+
 
 class classicAlien(Enemy):
-    def __init__(self, position):
-        super().__init__(position, "Images/Sprites/Enemies/classicAlien.png")
+    def __init__(self):
+        super().__init__("Images/Sprites/Enemies/classicAlien.png")
 
 class batBoss(Enemy):
-    def __init__(self, position):
-        super().__init__(position, "Images/Sprites/Enemies/batBass.png", size = (50, 50))
+    def __init__(self):
+        super().__init__("Images/Sprites/Enemies/batBass.png", size = (50, 50))
 
 class beeEnemy(Enemy):
     def __init__(self, position):
--- src/tools/unit_handler.py
+++ src/tools/unit_handler.py
@@ -7,6 +7,11 @@ import numpy
 from pygame.display import update
 
 
+def generate_relative_value_2d(value_2d):
+    value_2d = numpy.divide(value_2d, (100, 100))
+    value_2d = numpy.multiply(value_2d, pygame.display.get_surface().get_size())
+    return value_2d
+
 # SWTH is a custom unit that is measured in as screen_width/100. This enables for various screen sizes to be supported easily
 class swth_sprite(pygame.sprite.Sprite):
     def __init__(self, image):
@@ -14,19 +19,17 @@ class swth_sprite(pygame.sprite.Sprite):
         self.super = super()
         self.position = [0,0]
         self.image = image
-        self.image = pygame.transform.scale(image, self.generate_relative_value_2d((5,7)))
+        self.image = pygame.transform.scale(image, generate_relative_value_2d((5,7)))
         self.rect = self.image.get_rect()
 
 
     def generate_relative_coords(self):
         position = numpy.divide(self.position, (100,100))
         position = numpy.multiply(position, pygame.display.get_surface().get_size())
+        self.rect.center = position
         return position
 
-    def generate_relative_value_2d(self, value_2d):
-        value_2d = numpy.divide(value_2d, (100,100))
-        value_2d = numpy.multiply(value_2d, pygame.display.get_surface().get_size())
-        return value_2d
+
 
 
     def update_position(self, position):
@@ -35,8 +38,15 @@ class swth_sprite(pygame.sprite.Sprite):
     def get_position(self):
         return self.position
 
+    # def get_relative_position(self):
+    #     return self.generate_relative_coords()
+
     def get_image(self):
         return self.image
+
+    def get_rect(self):
+        return self.rect
+
 
     def draw(self):
         screen = pygame.display.get_surface()
@@ -45,4 +55,5 @@ class swth_sprite(pygame.sprite.Sprite):
         screen.blit(self.image, self.generate_relative_coords())
 
     def update(self):
+
         self.rect.center = self.generate_relative_coords()
